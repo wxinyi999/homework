5b9fc938d266de3235323d46a29401ef38a3b98c
wxinyi999 <62133497+wxinyi999@users.noreply.github.com>
2020-04-30 22:00:07 +0800

    Update settings.py

--- st2-master/students/settings.py
+++ st2-master/students/settings.py
@@ -35,9 +35,10 @@ from linebot.models import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
-# 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_ACCESS_TOKEN ='fy5uXqEUrqlLAAR3mh+8PNqOKZfCpz3jd4KSicqoTmELv16ExXNina+1+dtetd1cqsPmri6ifQRU079EW0u2DB12lECHq4u8XU3Wr/vobqCQAB8LpJSuhTsRtPvwpQFF7cA6DWvZFDQuEGzjd+oZCQdB04t89/1O/w1cDnyilFU=
+LINE_CHANNEL_SECRET='76fd1ad333c93425df7261f6d8e30ced
+
+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
